5fd42bc89d32c884d667cced7694d3d4e411b9bc
Leon Dail <leondailofficial@gmail.com>
2019-08-15 12:12:43 -0700

    added accessibility to cat carousel

--- cat-carousel/index.js
+++ cat-carousel/index.js
@@ -2,10 +2,27 @@
 
 function thumbnailClicks() {
   $('.thumbnail').on('click', e => {
-    const imgSrc = $(event.currentTarget).attr('src');
-    console.log(imgSrc);
-});
+    //find the attributes of the clicked image
+    const imgSelection = $(event.currentTarget).find('input');
+    const imgSrc = imgSelection.attr('src');
+    const imgAlt = imgSelection.attr('alt');
+    
+    //turn off all aria-pressed=true previously and set the selection to 'aria-pressed=true' on current -> for accessibility?
+    let otherSelections = $(event.currentTarget).not(imgSelection);
+    otherSelections.attr('aria-pressed',false);
+    imgSelection.attr('aria-pressed','true');
+
+
+    //set the attributes of the clicked image to the top image
+    $('.hero').find('img').attr('src',imgSrc).attr('alt',imgAlt);
+  });
 }
+
+$(thumbnailClicks);
+
+
+
+
 
 
 
